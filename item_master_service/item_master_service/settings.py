93ceaa691d96c56598e478032f84f097777cfe5b
Rupesh-Mistri <rupesh.kumar805@gmail.com>
2024-11-19 23:56:59 +0530

    t

--- item_master_service/item_master_service/settings.py
+++ item_master_service/item_master_service/settings.py
@@ -107,6 +107,7 @@ CORS_ALLOWED_ORIGINS = [
     # "https://example.com",
     # "https://sub.example.com",
     "http://localhost:3000",
+    "http://0.0.0.0:3000",
     # "http://127.0.0.1:9000",
 ]
 
